f40a8f75655035fb96aaa94d30e145e13bc8cd2f
cyan333 <iamcyanxie@gmail.com>
2016-11-19 16:21:39 -0500

    Scan Done 2

--- app/src/main/java/com/ecitypower/www/smartbms/ScanActivity.java
+++ app/src/main/java/com/ecitypower/www/smartbms/ScanActivity.java
@@ -4,50 +4,84 @@ import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 public class ScanActivity extends ListActivity {
     private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeService mBluetoothLeService;
+    private BLENameAddress connectedBLE;
+    private Handler mHandler;
+    private Button scanButton;
+    private static final long SCAN_PERIOD = 10000;
 
     ArrayList<BLENameAddress> BLEDevicesList=new ArrayList<BLENameAddress>();
-
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
     ArrayAdapter<BLENameAddress> listAdapter;
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(connectedBLE.getDeviceAddress());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        mHandler = new Handler();
 
-        Button button= (Button) findViewById(R.id.scanDevicesButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        scanButton = (Button) findViewById(R.id.scanDevicesButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i("debug", "Scan button Clicked");
+                scanButton.setText("扫描中...");
+                scanButton.setEnabled(false);
 //                new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1; 
-                mBluetoothAdapter.startLeScan(mLeScanCallback);
+                scanLeDevice();
             }
         });
 
         listAdapter = new ArrayAdapter<BLENameAddress>(this, android.R.layout.simple_list_item_1, BLEDevicesList);
         setListAdapter(listAdapter);
 
+
+
         //Bluetooth permission request.
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
         != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] { android.Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
         }
-
 
         // Use this check to determine whether BLE is supported on the device.  Then you can
         // selectively disable BLE-related features.
@@ -66,6 +100,36 @@ public class ScanActivity extends ListActivity {
             return;
         }
 
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        connectedBLE = BLEDevicesList.get(position);
+        Log.i("debug", "Connecting to " + connectedBLE.getDeviceName());
+
+//        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+//        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+//        mBluetoothLeService.connect(connectedBLE.getDeviceAddress());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    private void scanLeDevice() {
+        // Stops scanning after a pre-defined scan period.
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                scanButton.setText("扫描蓝牙设备");
+                scanButton.setEnabled(true);
+            }
+        }, SCAN_PERIOD);
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
     }
 
     // Device scan callback.
